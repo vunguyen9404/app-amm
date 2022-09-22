567db3bb4a741c8f602e68ada08ca0b5f3f3c63a
CetusDevelopment <sunnykim888@proton.me>
2022-09-22 20:37:26 +0800

    update

---
+++ dist/_nuxt/detail.b5283596.mjs
@@ -0,0 +1,1 @@
+import{ag as e}from"./entry.ceb8596e.mjs";const l=e((t,d)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{l as default};
--- dist/dist/server/_nuxt/detail.65c77691.js
+++
@@ -1,1 +0,0 @@
-import{F as e}from"../server.mjs";import"#internal/nitro";const c=e((t,r)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{c as default};
