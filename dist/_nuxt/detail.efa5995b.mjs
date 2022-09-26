9ff221d487503fd11b2e80786152e31996a4b54e
CetusDevelopment <sunnykim888@proton.me>
2022-09-26 19:28:21 +0800

    update

---
+++ dist/_nuxt/detail.efa5995b.mjs
@@ -0,0 +1,1 @@
+import{ag as e}from"./entry.623f1da2.mjs";const l=e((t,d)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{l as default};
--- dist/_nuxt/index.a97287db.mjs
+++
@@ -1,1 +0,0 @@
-import o from"./swap.5349483a.mjs";import"./refresh-icon.1b08bd96.mjs";import"./entry.a6b8ef14.mjs";import"./assets-card.5b45be8b.mjs";import"./coin-select.809cdbd8.mjs";import"./img-leverage_2x.0c8422f1.mjs";import"./swap-setting.64760262.mjs";const s=o;export{s as default};
