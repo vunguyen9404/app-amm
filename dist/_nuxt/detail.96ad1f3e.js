0fea834b03dea8d76977b492ff4c90a62c0a838e
CetusDevelopment <sunnykim888@proton.me>
2022-11-17 16:12:22 +0800

    update

---
+++ dist/_nuxt/detail.96ad1f3e.js
@@ -0,0 +1,1 @@
+import{az as e}from"./entry.d5059ee4.js";const a=e((t,r)=>{const o=document.querySelector(".body-container-bg");o&&o.scrollTo({top:0})});export{a as default};
--- dist/_nuxt/detail.b7e0aa97.js
+++
@@ -1,1 +0,0 @@
-import{az as e}from"./entry.58689cec.js";const a=e((t,r)=>{const o=document.querySelector(".body-container-bg");o&&o.scrollTo({top:0})});export{a as default};
